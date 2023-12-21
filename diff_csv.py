b7e1650fd88259cfb2f9ad186e25d9a218ca61e2
Isaac <42228727+isaacchook@users.noreply.github.com>
2023-12-21 21:43:01 +0800

    Simple csv compare method

--- diff_csv.py
+++ diff_csv.py
@@ -2,18 +2,14 @@
 import pandas as pd
 
 #load the file 
-file1 = 'file1.csv'
-file2 = 'file2.csv'
+file1 = 'Oct.csv'
+file2 = 'Nov.csv'
 
 #declare datafames
 df1 = pd.read_csv(file1)
 df2 = pd.read_csv(file2)
 
 #compare them
-compare_csv = df1.equals(df2)
-
-#conditional statements
-if compare_csv:
-    print('They are identical') #expected because file1=file2
-else:
-    print('They are different')
+compare_csv = df1.compare(df2)
+compare_csv.rename(columns={'self': 'Oct', 'other': 'Nov'}, inplace=True)
+print(compare_csv)
